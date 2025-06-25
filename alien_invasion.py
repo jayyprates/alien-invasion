b4c007d6cc9e1561a9a87ba48e4e84768314488f
jayyprates <joanaprates2@gmail.com>
2025-06-25 14:58:22 -0300

    ship movement and refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,13 +24,8 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-
-            # Redraw the screen during each pass through the loop. 
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
+            self.ship.update()
+            self._update_screen()
             self.clock.tick(60)
 
     def _check_events(self):
@@ -38,6 +33,24 @@ class AlienInvasion:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True 
+
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
